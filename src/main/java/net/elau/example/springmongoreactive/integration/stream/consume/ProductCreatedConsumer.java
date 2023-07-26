be7d5ee1030b02eac54545b17e0db323b35f358b
elau <elauso@gmail.com>
2023-07-26 02:26:23 -0300

    Testando doOnNext()

--- src/main/java/net/elau/example/springmongoreactive/integration/stream/consume/ProductCreatedConsumer.java
+++ src/main/java/net/elau/example/springmongoreactive/integration/stream/consume/ProductCreatedConsumer.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import net.elau.example.springmongoreactive.integration.stream.event.ProductCreatedEvent;
 import net.elau.example.springmongoreactive.mapper.ProductMapper;
 import net.elau.example.springmongoreactive.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -13,12 +15,17 @@ import java.util.function.Consumer;
 @RequiredArgsConstructor
 public class ProductCreatedConsumer {
 
+    private static final Logger log = LoggerFactory.getLogger(ProductCreatedConsumer.class);
+
     private final ProductMapper mapper;
 
     private final ProductService service;
 
     @Bean
     public Consumer<ProductCreatedEvent> productCreatedConsume() {
-        return (event) -> service.create(mapper.toDto(event)).subscribe();
+        return (event) ->
+                service.create(mapper.toDto(event))
+                        .doOnNext(p -> log.debug("Product registered: {}", p))
+                        .subscribe();
     }
 }
